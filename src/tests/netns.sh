708338d534083d569d95222283447756c7319b46
Jason A. Donenfeld <Jason@zx2c4.com>
2016-07-25 14:17:11 +0200

    tests: use makefile and expand greatly
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Yogesh Sharma <kingyogesh04@gmail.com>

--- src/tests/netns.sh
+++ src/tests/netns.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+<<<<<<< HEAD
 # SPDX-License-Identifier: GPL-2.0
 #
 # Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
 #
 # This script tests the below topology:
 #
+=======
+
+# This script tests the below topology:
+#
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 # ┌─────────────────────┐   ┌──────────────────────────────────┐   ┌─────────────────────┐
 # │   $ns1 namespace    │   │          $ns0 namespace          │   │   $ns2 namespace    │
 # │                     │   │                                  │   │                     │
@@ -12,6 +18,7 @@
 # ││  wg0   │───────────┼───┼────────────│   lo   │────────────┼───┼───────────│  wg0   ││
 # │├────────┴──────────┐│   │    ┌───────┴────────┴────────┐   │   │┌──────────┴────────┤│
 # ││192.168.241.1/24   ││   │    │(ns1)         (ns2)      │   │   ││192.168.241.2/24   ││
+<<<<<<< HEAD
 # ││fd00::1/24         ││   │    │127.0.0.1:1   127.0.0.1:2│   │   ││fd00::2/24         ││
 # │└───────────────────┘│   │    │[::]:1        [::]:2     │   │   │└───────────────────┘│
 # └─────────────────────┘   │    └─────────────────────────┘   │   └─────────────────────┘
@@ -20,16 +27,30 @@
 # After the topology is prepared we run a series of TCP/UDP iperf3 tests between the
 # wireguard peers in $ns1 and $ns2. Note that $ns0 is the endpoint for the wg0
 # interfaces in $ns1 and $ns2. See https://www.wireguard.com/netns/ for further
+=======
+# ││abcd::1/24         ││   │    │127.0.0.1:1   127.0.0.1:2│   │   ││abcd::2/24         ││
+# │└───────────────────┘│   │    │[::]:1        [::]:2     │   │   │└───────────────────┘│
+# └─────────────────────┘   │    └─────────────────────────┘   │   └─────────────────────┘
+#                           └──────────────────────────────────┘                          
+#
+# After the topology is prepared we run a series of TCP/UDP iperf3 tests between the
+# wireguard peers in $ns1 and $ns2. Note that $ns0 is the endpoint for the wg0
+# interfaces in $ns1 and $ns2. See https://www.wireguard.io/netns/ for further
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 # details on how this is accomplished.
 set -e
 
 exec 3>&1
+<<<<<<< HEAD
 export LANG=C
+=======
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 export WG_HIDE_KEYS=never
 netns0="wg-test-$$-0"
 netns1="wg-test-$$-1"
 netns2="wg-test-$$-2"
 pretty() { echo -e "\x1b[32m\x1b[1m[+] ${1:+NS$1: }${2}\x1b[0m" >&3; }
+<<<<<<< HEAD
 compatwarn() { echo -e "\x1b[31m\x1b[1m[-] Tolerated failure: $1\x1b[0m" >&3; }
 pp() { pretty "" "$*"; "$@"; }
 maybe_exec() { if [[ $BASHPID -eq $$ ]]; then "$@"; else exec "$@"; fi; }
@@ -43,33 +64,60 @@ sleep() { read -t "$1" -N 1 || true; }
 waitiperf() { pretty "${1//*-}" "wait for iperf:${3:-5201} pid $2"; while [[ $(ss -N "$1" -tlpH "sport = ${3:-5201}") != *\"iperf3\",pid=$2,fd=* ]]; do sleep 0.1; done; }
 waitncatudp() { pretty "${1//*-}" "wait for udp:1111 pid $2"; while [[ $(ss -N "$1" -ulpH 'sport = 1111') != *\"ncat\",pid=$2,fd=* ]]; do sleep 0.1; done; }
 waitiface() { pretty "${1//*-}" "wait for $2 to come up"; ip netns exec "$1" bash -c "while [[ \$(< \"/sys/class/net/$2/operstate\") != up ]]; do read -t .1 -N 0 || true; done;"; }
+=======
+pp() { pretty "" "$*"; "$@"; }
+n0() { pretty 0 "$*"; ip netns exec $netns0 "$@"; }
+n1() { pretty 1 "$*"; ip netns exec $netns1 "$@"; }
+n2() { pretty 2 "$*"; ip netns exec $netns2 "$@"; }
+ip0() { pretty 0 "ip $*"; ip -n $netns0 "$@"; }
+ip1() { pretty 1 "ip $*"; ip -n $netns1 "$@"; }
+ip2() { pretty 2 "ip $*"; ip -n $netns2 "$@"; }
+sleep() { read -t "$1" -N 0 || true; }
+waitiperf() { pretty "${1//*-}" "wait for iperf:5201"; while [[ $(ss -N "$1" -tlp 'sport = 5201') != *iperf3* ]]; do sleep 0.1; done; }
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 
 cleanup() {
 	set +e
 	exec 2>/dev/null
+<<<<<<< HEAD
 	printf "$orig_message_cost" > /proc/sys/net/core/message_cost
+=======
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 	ip0 link del dev wg0
 	ip0 link del dev wg1
 	ip1 link del dev wg0
 	ip1 link del dev wg1
 	ip2 link del dev wg0
+<<<<<<< HEAD
 	ip2 link del dev wg1
 	local to_kill="$(ip netns pids $netns0) $(ip netns pids $netns1) $(ip netns pids $netns2)"
 	[[ -n $to_kill ]] && kill $to_kill
 	pp ip netns del $netns1
 	pp ip netns del $netns2
 	pp ip netns del $netns0
+=======
+	pp ip netns del $netns1
+	pp ip netns del $netns2
+	pp ip netns del $netns0
+	kill -- -$$
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 	exit
 }
 
 orig_message_cost="$(< /proc/sys/net/core/message_cost)"
 trap cleanup EXIT
+<<<<<<< HEAD
 printf 0 > /proc/sys/net/core/message_cost
+=======
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 
 ip netns del $netns0 2>/dev/null || true
 ip netns del $netns1 2>/dev/null || true
 ip netns del $netns2 2>/dev/null || true
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 pp ip netns add $netns0
 pp ip netns add $netns1
 pp ip netns add $netns2
@@ -79,6 +127,7 @@ ip0 link add dev wg0 type wireguard
 ip0 link set wg0 netns $netns1
 ip0 link add dev wg0 type wireguard
 ip0 link set wg0 netns $netns2
+<<<<<<< HEAD
 <<<<<<< HEAD
 key1="$(pp wg genkey)"
 key2="$(pp wg genkey)"
@@ -115,6 +164,44 @@ configure_peers() {
 	ip2 link set up dev wg0
 }
 configure_peers
+=======
+key1="$(pp wg genkey)"
+key2="$(pp wg genkey)"
+pub1="$(pp wg pubkey <<<"$key1")"
+pub2="$(pp wg pubkey <<<"$key2")"
+psk="$(pp wg genpsk)"
+[[ -n $key1 && -n $key2 && -n $psk ]]
+
+configure_peers() {
+	ip1 addr add 192.168.241.1/24 dev wg0
+	ip1 addr add abcd::1/24 dev wg0
+
+	ip2 addr add 192.168.241.2/24 dev wg0
+	ip2 addr add abcd::2/24 dev wg0
+
+	n1 wg set wg0 \
+		private-key <(echo "$key1") \
+		preshared-key <(echo "$psk") \
+		listen-port 1 \
+		peer "$pub2" \
+			allowed-ips 192.168.241.2/32,abcd::2/128
+	n2 wg set wg0 \
+		private-key <(echo "$key2") \
+		preshared-key <(echo "$psk") \
+		listen-port 2 \
+		peer "$pub1" \
+			allowed-ips 192.168.241.1/32,abcd::1/128
+
+	ip1 link set up dev wg0
+	ip2 link set up dev wg0
+}
+configure_peers
+
+tests() {
+	# Status before
+	n1 wg
+	n2 wg
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 
 tests() {
 	# Ping over IPv4
@@ -128,6 +215,7 @@ tests() {
 
 	# TCP over IPv4
 	n2 iperf3 -s -1 -B 192.168.241.2 &
+<<<<<<< HEAD
 	waitiperf $netns2 $!
 	n1 iperf3 -Z -t 3 -c 192.168.241.2
 
@@ -161,6 +249,29 @@ tests() {
 		done
 		wait "${pids[@]}"
 	done
+=======
+	waitiperf $netns2
+	n1 iperf3 -Z -i 1 -n 500M "$@" -c 192.168.241.2
+
+	# TCP over IPv6
+	n1 iperf3 -s -1 -B abcd::1 &
+	waitiperf $netns1
+	n2 iperf3 -Z -i 1 -n 500M "$@" -c abcd::1
+
+	# UDP over IPv4
+	n1 iperf3 -s -1 -B 192.168.241.1 &
+	waitiperf $netns1
+	n2 iperf3 -Z -i 1 -n 500M "$@" -b 0 -u -c 192.168.241.1
+
+	# UDP over IPv6
+	n2 iperf3 -s -1 -B abcd::2 &
+	waitiperf $netns2
+	n1 iperf3 -Z -i 1 -n 500M "$@" -b 0 -u -c abcd::2
+
+	# Status after
+	n1 wg
+	n2 wg
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 }
 
 [[ $(ip1 link show dev wg0) =~ mtu\ ([0-9]+) ]] && orig_mtu="${BASH_REMATCH[1]}"
@@ -169,6 +280,7 @@ big_mtu=$(( 34816 - 1500 + $orig_mtu ))
 # Test using IPv4 as outer transport
 n1 wg set wg0 peer "$pub2" endpoint 127.0.0.1:2
 n2 wg set wg0 peer "$pub1" endpoint 127.0.0.1:1
+<<<<<<< HEAD
 # Before calling tests, we first make sure that the stats counters and timestamper are working
 n2 ping -c 10 -f -W 1 192.168.241.1
 { read _; read _; read _; read rx_bytes _; read _; read tx_bytes _; } < <(ip2 -stats link show dev wg0)
@@ -185,6 +297,8 @@ read _ timestamp < <(n1 wg show wg0 latest-handshakes)
 tests
 ip1 link set wg0 mtu $big_mtu
 ip2 link set wg0 mtu $big_mtu
+=======
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 tests
 
 ip1 link set wg0 mtu $orig_mtu
@@ -193,6 +307,7 @@ ip2 link set wg0 mtu $orig_mtu
 # Test using IPv6 as outer transport
 n1 wg set wg0 peer "$pub2" endpoint [::1]:2
 n2 wg set wg0 peer "$pub1" endpoint [::1]:1
+<<<<<<< HEAD
 tests
 ip1 link set wg0 mtu $big_mtu
 ip2 link set wg0 mtu $big_mtu
@@ -214,19 +329,31 @@ n0 iptables -F INPUT
 ip1 link set wg0 mtu $orig_mtu
 ip2 link set wg0 mtu $orig_mtu
 
+=======
+tests
+
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 # Test using IPv4 that roaming works
 ip0 -4 addr del 127.0.0.1/8 dev lo
 ip0 -4 addr add 127.212.121.99/8 dev lo
 n1 wg set wg0 listen-port 9999
 n1 wg set wg0 peer "$pub2" endpoint 127.0.0.1:2
+<<<<<<< HEAD
 n1 ping6 -W 1 -c 1 fd00::2
 [[ $(n2 wg show wg0 endpoints) == "$pub1	127.212.121.99:9999" ]]
+=======
+n1 ping6 -W 1 -c 1 abcd::2
+[[ $(n2 wg show wg0 endpoints) == "$pub1	127.212.121.99:9999" ]]
+n1 wg
+n2 wg
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 
 # Test using IPv6 that roaming works
 n1 wg set wg0 listen-port 9998
 n1 wg set wg0 peer "$pub2" endpoint [::1]:2
 n1 ping -W 1 -c 1 192.168.241.2
 [[ $(n2 wg show wg0 endpoints) == "$pub1	[::1]:9998" ]]
+<<<<<<< HEAD
 
 # Test that crypto-RP filter works
 n1 wg set wg0 peer "$pub2" allowed-ips 192.168.241.0/24
@@ -287,11 +414,16 @@ ip2 link del wg1
 
 ip0 link del wg1
 ip1 link del wg0
+=======
+n1 wg
+n2 wg
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 
 # Test using NAT. We now change the topology to this:
 # ┌────────────────────────────────────────┐    ┌────────────────────────────────────────────────┐     ┌────────────────────────────────────────┐
 # │             $ns1 namespace             │    │                 $ns0 namespace                 │     │             $ns2 namespace             │
 # │                                        │    │                                                │     │                                        │
+<<<<<<< HEAD
 # │  ┌─────┐             ┌─────┐           │    │    ┌──────┐              ┌──────┐              │     │  ┌─────┐            ┌─────┐            │
 # │  │ wg0 │─────────────│vethc│───────────┼────┼────│vethrc│              │vethrs│──────────────┼─────┼──│veths│────────────│ wg0 │            │
 # │  ├─────┴──────────┐  ├─────┴──────────┐│    │    ├──────┴─────────┐    ├──────┴────────────┐ │     │  ├─────┴──────────┐ ├─────┴──────────┐ │
@@ -300,6 +432,22 @@ ip1 link del wg0
 # │  └────────────────┘  └────────────────┘│    │    └────────────────┘    └───────────────────┘ │     │  └────────────────┘ └────────────────┘ │
 # └────────────────────────────────────────┘    └────────────────────────────────────────────────┘     └────────────────────────────────────────┘
 
+=======
+# │                                        │    │                                                │     │                                        │
+# │  ┌─────┐             ┌─────┐           │    │    ┌──────┐              ┌──────┐              │     │  ┌─────┐            ┌─────┐            │
+# │  │ wg0 │─────────────│vethc│───────────┼────┼────│vethrc│              │vethrs│──────────────┼─────┼──│veths│────────────│ wg0 │            │
+# │  ├─────┴──────────┐  ├─────┴──────────┐│    │    ├──────┴─────────┐    ├──────┴────────────┐ │     │  ├─────┴──────────┐ ├─────┴──────────┐ │
+# │  │192.168.241.1/24│  │192.168.1.100/24││    │    │192.168.1.100/24│    │10.0.0.1/24        │ │     │  │10.0.0.100/24   │ │192.168.241.2/24│ │
+# │  │abcd::1/24      │  │                ││    │    │                │    │SNAT:192.168.1.0/24│ │     │  │                │ │abcd::2/24      │ │
+# │  └────────────────┘  └────────────────┘│    │    └────────────────┘    └───────────────────┘ │     │  └────────────────┘ └────────────────┘ │
+# │                                        │    │                                                │     │                                        │
+# │                                        │    │                                                │     │                                        │
+# │                                        │    │                                                │     │                                        │
+# └────────────────────────────────────────┘    └────────────────────────────────────────────────┘     └────────────────────────────────────────┘
+
+ip1 link del wg0
+ip2 link del wg0
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 ip1 link add dev wg0 type wireguard
 ip2 link add dev wg0 type wireguard
 configure_peers
@@ -317,6 +465,7 @@ ip1 link set vethc up
 ip1 route add default via 192.168.1.1
 ip2 addr add 10.0.0.100/24 dev veths
 ip2 link set veths up
+<<<<<<< HEAD
 waitiface $netns0 vethrc
 waitiface $netns0 vethrs
 waitiface $netns1 vethc
@@ -330,10 +479,27 @@ n0 iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -d 10.0.0.0/24 -j SNAT --to 
 n1 wg set wg0 peer "$pub2" endpoint 10.0.0.100:2 persistent-keepalive 1
 n1 ping -W 1 -c 1 192.168.241.2
 n2 ping -W 1 -c 1 192.168.241.1
+=======
+
+n0 bash -c 'echo 1 > /proc/sys/kernel/sysctl_writes_strict'
+n0 bash -c 'echo 1 > /proc/sys/net/ipv4/ip_forward'
+n0 bash -c 'echo 2 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout'
+n0 bash -c 'echo 2 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout_stream'
+n0 iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -d 10.0.0.0/24 -j SNAT --to 10.0.0.1
+
+n1 wg set wg0 peer "$pub2" endpoint 10.0.0.100:2 persistent-keepalive 1
+n1 wg
+n2 wg
+n1 ping -W 1 -c 1 192.168.241.2
+n2 ping -W 1 -c 1 192.168.241.1
+n1 wg
+n2 wg
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
 [[ $(n2 wg show wg0 endpoints) == "$pub1	10.0.0.1:1" ]]
 # Demonstrate n2 can still send packets to n1, since persistent-keepalive will prevent connection tracking entry from expiring (to see entries: `n0 conntrack -L`).
 pp sleep 3
 n2 ping -W 1 -c 1 192.168.241.1
+<<<<<<< HEAD
 n1 wg set wg0 peer "$pub2" persistent-keepalive 0
 
 # Test that sk_bound_dev_if works
@@ -644,3 +810,5 @@ for object in "${!objects[@]}"; do
 done
 [[ $alldeleted -eq 1 ]]
 pretty "" "Objects that were created were also destroyed."
+=======
+>>>>>>> 2b3882d8f471... tests: use makefile and expand greatly
